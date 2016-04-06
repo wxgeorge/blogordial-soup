db07dad12122baf3ee5a8fe3f76e779ce2f19598
Wesley George <wesley.george@gmail.com>
2016-04-06 00:28:08 -0700

    nested functions don't exist in ruby, but do in python

---
+++ ruby-quirks/me.py
@@ -0,0 +1,7 @@
+def fun1():
+    print "This is fun1 running!"
+    def fun2():
+        print "this is fun2 running!"
+
+fun1()
+fun2()
---
+++ ruby-quirks/me.rb
@@ -0,0 +1,9 @@
+def fun1()
+  puts "This is fun1 running"
+  def fun2()
+    puts "This is fun2 running"
+  end
+end
+
+fun1()
+fun2()
